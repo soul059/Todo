f14f984e333dc1e679fb5e7bfe766876e89ba5ee
soul059 <keval059@outlook.com>
2024-02-26 21:51:25 +0530

    TO-DO web app app

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect, useRef } from 'react'
+import Navbar from './components/navbar'
+import Todo from './components/todo'
+import './App.css'
+
+let Todos = []
+
+function App() {
+
+  const [todos, newtodo] = useState([])
+  const [done, setdone] = useState(false)
+
+  const count = localStorage.getItem('count') ? useRef(localStorage.getItem('count')) : useRef(0)
+  
+  const creat = useRef()
+
+  
+  let creatTodo = () => {
+    const x = creat.current.value
+    count.current++
+    if (x) {
+      newtodo([...todos, { text: x, istick: false, id: count.current }])
+      // localStorage.setItem('todos', JSON.stringify([...todos, { text: x, istick: false, id: count.current }]))
+      Todos.push({ text: x, istick: false, id: count.current })
+      localStorage.setItem('count', count.current)
+      creat.current.value = ""
+    }
+  }
+  useEffect(() => {
+    if (localStorage.getItem('todos')) {
+      newtodo(JSON.parse(localStorage.getItem('todos')))
+      Todos = JSON.parse(localStorage.getItem('todos'))
+    }
+  },[])
+
+  useEffect(() => {
+    Todos = todos
+    let x = Todos.indexOf(Todos.find((todo)=> todo.text === "" ))
+    if (x != -1) {
+      Todos.splice(x,1)
+    }
+  })
+
+  useEffect(() => {
+    
+  },[done])
+  
+
+  return (
+    <>
+      <Navbar />
+      <main className=''>
+        <section className={"flex flex-col text-center shadow-lg bg-amber-300 h-96 w-80 mx-auto my-10 rounded-lg"}>
+          <span className='text-lg'>Add TODO</span>
+          <div className='mt-10 flex flex-row gap-2 mx-auto'>
+            <input className='rounded-md border-solid border-2 border-cyan-500 shadow-md p-1' type="text" ref={creat} />
+            <button className='bg-orange-400 p-1 rounded-lg shadow-lg' onClick={() => creatTodo()} >Creat</button>
+          </div>
+          <button className='bg-orange-400 p-1 rounded-lg w-2/3 mx-auto mt-5 shadow-md mb-2' onClick={()=>done ? setdone(false) : setdone(true)}>Show {done?"not done":"done"}</button>
+          <hr />
+          <span className='underline'>Your Todos</span>
+          {/* {console.log(todos)} */}
+          <div className='flex flex-col gap-2 mt-2 '>
+            {todos.map((todo) => {
+              
+              if (done == false && todo.istick == false) {
+                
+                return (
+                  <Todo key={todo.id} todo={todo} status={done}/>
+                  )
+              }
+              else if (done == true && todo.istick == true){
+                return (
+                  <Todo key={todo.id} todo={todo} status={done}/>
+                  )
+              }
+              else {
+                return <></>
+              }
+            })}
+          </div>
+          {console.log(todos)}
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
+export {Todos}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav className={"p-2 bg-cyan-700 rounded-lg sticky top-1 w-1/2 mx-auto text-center text-white"}>
+        <span>To-Do list</span>
+    </nav>
+  )
+}
+
+export default navbar
